9b5a90aef059aa8947d0981d63cd1fce3cdb0ad4
Lukong123 <70434467+Lukong123@users.noreply.github.com>
2021-05-30 06:28:18 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-//js cose
+//js code
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
